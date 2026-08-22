9b748dee6269592dcda5e77fff98ebf314383634
Jordan <escalj2@gmail.com>
2026-08-22 11:51:34 -0700

    test commit

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 
-
-"""diff_msg = subprocess.run(["git", "diff", "--staged" ], capture_output=True, text=True)
-if diff_msg.stdout == '':
-    print("Nothing currently staged")
-print(diff_msg)"""
-
+import subprocess
 from google import genai
 import os
 from dotenv import load_dotenv
 
-# Load the .env file
+diff_msg = subprocess.run(["git", "diff", "--staged" ], capture_output=True, text=True)
+if diff_msg.stdout == '':
+    print("Nothing currently staged")
+print(diff_msg)
+
+# Load the .env file and access the key
 load_dotenv()
-
-# Access the key using os.getenv()
 api_key = os.getenv("GEMINI_API_KEY")
-
 
 client = genai.Client(api_key=api_key)
 
 interaction = client.interactions.create(
     model="gemini-3.7-flash",
-    input="Explain how AI works in a few words"
+    input="Generate a git commit message based on this git-diff --staged output:" + diff_msg
 )
 print(interaction.output_text)
+
+
+#test change
