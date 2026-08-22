e5725b9080f0f85a9a5ebf9a129256e2695ddce0
Jordan <escalj2@gmail.com>
2026-08-22 11:35:51 -0700

    load .env file and access key using dotenv. Use api key to send test interaction to api

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-import subprocess
 
-diff_msg = subprocess.run(["git", "diff", "--staged" ], capture_output=True, text=True)
+
+"""diff_msg = subprocess.run(["git", "diff", "--staged" ], capture_output=True, text=True)
 if diff_msg.stdout == '':
     print("Nothing currently staged")
-print(diff_msg)
+print(diff_msg)"""
+
+from google import genai
+import os
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+# Access the key using os.getenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+
+client = genai.Client(api_key=api_key)
+
+interaction = client.interactions.create(
+    model="gemini-3.7-flash",
+    input="Explain how AI works in a few words"
+)
+print(interaction.output_text)
